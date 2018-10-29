582943bd79ad06b47346849ed3a678d580d08d11
Derek Xiao <derek.xiao77@gmail.com>
2018-10-29 16:36:05 -0400

    working dash

--- QUEST/wine_dash.py
+++ QUEST/wine_dash.py
@@ -6,16 +6,23 @@ import plotly.graph_objs as go
 
 app = dash.Dash(__name__)
 
-raw_data = pd.read_excel('Marketing Research Restaurant Data.xlsx')
+wine_data = pd.read_csv('wine_data.csv')
+clean_wine_data = wine_data.dropna(subset=["price","points","variety","country","region_1"]).copy(deep=True)
+
+## Create list of countries for dropdown
+countries_list = clean_wine_data['country'].unique()
+countries_dict_list = []
+for i in countries_list:
+    countries_dict_list.append({'label':i,'value':i})
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+app.layout = html.Div(children=[
     html.H1(
-        children='Hello Dash',
+        children='Wine Dashboard',
         style={
             'textAlign': 'center',
             'color': colors['text']
@@ -29,50 +36,67 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 
     html.Label('Dropdown'),
     dcc.Dropdown(
-        id = 'dropdown',
-        options=[
-            {'label': 'Indian', 'value': 'Indian'},
-            {'label': 'American', 'value': 'American'},
-            {'label': 'French', 'value': 'French'},
-            {'label': 'Mexican', 'value': 'Mexican'},
-            {'label': 'Chinese', 'value': 'Chiense'},
-            {'label': 'Italian', 'value': 'Italian'},
-            {'label': 'Greek', 'value': 'Greek'}
-        ],
-        value=['Indian'],
+        id = 'country',
+        options=countries_dict_list,
+        value=['US'],
+        multi=True
+    ),
+    dcc.Dropdown(
+        id = 'region_1',
         multi=True
     ),
 
     dcc.Graph(
-        id='example-graph-2'
+        id='value-graph'
     )
 ])
 
+## Change the region list based off the selected country
 @app.callback(
-    dash.dependencies.Output(component_id='example-graph-2', component_property='figure'),
-    [dash.dependencies.Input(component_id='dropdown', component_property='value')]
+    dash.dependencies.Output(component_id='region_1', component_property='options'),
+    [dash.dependencies.Input(component_id='country', component_property='value')]
+)
+def refresh_regions(selected_country):
+    filtered_countries = clean_wine_data[clean_wine_data['country'].isin(selected_country)] ## isin because it's checking series against list
+    regions_list = filtered_countries['region_1'].unique()
+    regions_dict_list = []
+    for i in regions_list:
+        regions_dict_list.append({'label':i,'value':i})
+
+    return regions_dict_list
+
+## Change list of wines based off country selected
+@app.callback(
+    ## component id is the id of the object you want to change
+    ## component_property is the property of the object that you want to change
+    dash.dependencies.Output(component_id='value-graph', component_property='figure'),
+    ## Output will change based off what Input is
+    [dash.dependencies.Input(component_id='country', component_property='value')]
 )
 def update_figure(selected_location):
     traces = []
     for i in range(len(selected_location)):
-        filtered_df = raw_data[raw_data['Q1 - Restaurant Type'] == selected_location[i]]
+        filtered_df = clean_wine_data[clean_wine_data['country'] == selected_location[i]]
+        agg_data = filtered_df.groupby('variety')['price','points'].mean()
         traces.append(go.Scatter(
-            x= filtered_df['Q2 - Location'].unique(),
-            y= filtered_df.groupby('Q2 - Location')['Q6 - Annual Sales'].mean(),
+            ## x-axis = price
+            x= agg_data['price'],
+            y= agg_data['points'],
             mode='markers',
             opacity=0.7,
             marker={
                 'size': 15,
                 'line': {'width': 0.5, 'color': 'white'}
             },
-            name=selected_location[i],
+            name= selected_location[i],
+            text = agg_data.index.tolist()
         ))
 
     return {
         'data': traces,
         'layout': go.Layout(
-            xaxis={'title': 'Restaurant Location'},
-            yaxis={'title': 'Average Sales'},
+            xaxis={'title': 'Average Price'},
+            yaxis={'title': 'Average Quality'},
             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             legend={'x': 0, 'y': 1},
             hovermode='closest'
